3dfb521a606bd423b2381a74d164f604bfccd38b
Ash <ecbuhendwa@gmail.com>
2022-04-05 17:05:07 -0400

    Hardhat Configuration & Creation of NFT.sol

---
+++ nft-marketplace/hardhat.config.js
@@ -0,0 +1,49 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      // Infura
+      url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    },
+    matic: {
+      // Infura
+      url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    }
+    
+
+
+    /*
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [process.env.privateKey]
+    },
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [process.env.privateKey]
+    }
+    */
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
