871c16d31a1b4479dc5c986257e74291480ed7e7
Ash <ecbuhendwa@gmail.com>
2022-04-07 15:08:16 -0400

    Update NFTMarket.sol : adding missing functions

--- nft-marketplace/contracts/NFTMarket.sol
+++ nft-marketplace/contracts/NFTMarket.sol
@@ -77,4 +77,83 @@ contract NFTMarket is ReentrancyGuard {
             );
 
     }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+    ) public payable nonReentrant {
+        unint price = idToMarketItem[itemId].price;
+        unint tokenId = idToMarketItem[itemId].tokenId;
+        require(msg.value, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transer(msg.value); //buyer pays the seller
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId); // nft transferred to buyer
+        idToMarketItem[itemId].owner = payable(msg.sender); //buyer becomes nft owner
+        idToMarketItem[itemId].sold = true; //buyer becomes mrktItem owner
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItem);
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i+1].owner == address(0)) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex +=1;
+            }
+        }
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount +=1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex +=1;
+            }
+        }
+        return items;
+    }
+
+    function fetchItemsCreated() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].seller == msg.sender) {
+                itemCount +=1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].seller == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex +=1;
+            }
+        }
+        return items;
+    }
 }
